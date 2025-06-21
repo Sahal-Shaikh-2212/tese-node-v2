74e525c6e587310ce65925ace251d4a7e733069d
Sahal-Shaikh-2212 <sahalshaikh2212@gmail.com>
2025-06-21 16:55:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Subscribe to Arpan Neupane's channel");
 });
-
+app.get("/about", (req, res) => {
+  res.send("you are on about page");
+});
 app.listen(port, () => {
   console.log("Server started on port 8080");
 });
